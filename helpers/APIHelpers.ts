cd8c9cbb6e14487eb28f5a7ff5a64665ece27aee
Lucy Llewellyn <lucy.llewellyn@yahoo.co.uk>
2023-10-02 12:14:11 +0100

    created api helpers file

---
+++ helpers/APIHelpers.ts
@@ -0,0 +1,25 @@
+import { APIRequestContext, expect } from "@playwright/test";
+import { authLoginPostRequest, roomDeleteRequest, roomGetRequest, roomPostRequest } from "./APIBuilders";
+
+export async function createRoom(request:APIRequestContext): Promise<number>{
+    const newRoom = await roomPostRequest(request);
+    expect((newRoom).status()).toEqual(201);
+    const resp = JSON.parse(await newRoom.text());
+    return resp.roomid;
+}
+
+export async function deleteRoom(request:APIRequestContext, roomId: number) {
+    const deleteRoom = await roomDeleteRequest(request, roomId)
+    expect(deleteRoom.ok()).toBeTruthy();
+
+    const retriveRoom = await roomGetRequest(request, roomId);
+    expect(retriveRoom.status()).toBe(500);
+}
+
+export async function login(request:APIRequestContext): Promise<string> {
+    const loginResponse = await authLoginPostRequest(request);
+    expect((loginResponse).status()).toEqual(200);
+    const headers = (loginResponse).headers();
+    const cookies = headers['set-cookie']
+    return cookies.split('=')[1].split(';')[0];
+}
